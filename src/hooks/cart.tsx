950fa443fee31d05ed8151ba3b323a61c157dc62
Iwry Rozemberg <iwryrozemberg@gmail.com>
2020-09-15 01:40:56 -0300

    Resolução do desafio

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,23 +30,93 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const productsCart = await AsyncStorage.getItem(
+        '@GoMarketplace:products',
+      );
+
+      if (productsCart) {
+        setProducts(JSON.parse(productsCart));
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const increment = useCallback(
+    async id => {
+      setProducts(prevState => {
+        const newState = prevState.map((product: Product) => {
+          if (product.id === id) {
+            const newQuantity = product.quantity + 1;
+            return { ...product, quantity: newQuantity };
+          }
+          return product;
+        });
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+        return newState;
+      });
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(products),
+      );
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const productFind = products.find(prod => prod.id === id);
+      if (productFind && productFind.quantity === 1) {
+        if (products.length === 1) {
+          setProducts([]);
+        } else {
+          const newProducts = products.filter(
+            product => product !== productFind,
+          );
+          setProducts(newProducts);
+        }
+      } else {
+        setProducts(prevState => {
+          const newState = prevState.map((product: Product) => {
+            if (product.id === id) {
+              const newQuantity = product.quantity - 1;
+              return { ...product, quantity: newQuantity };
+            }
+
+            return product;
+          });
+
+          return newState;
+        });
+      }
+
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(products),
+      );
+    },
+    [products],
+  );
+
+  const addToCart = useCallback(
+    async product => {
+      const productFind = products.find(prod => prod.id === product.id);
+
+      if (productFind) {
+        increment(productFind.id);
+        return;
+      }
+
+      setProducts(prevState => [...prevState, { ...product, quantity: 1 }]);
+
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(products),
+      );
+    },
+    [increment, products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,31 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, currentProduct) => {
+      const { price, quantity } = currentProduct;
 
-    return formatValue(0);
+      return acc + quantity * price;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, currentProduct) => {
+      const { quantity } = currentProduct;
 
-    return 0;
+      return acc + quantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
@@ -68,7 +76,7 @@ const Cart: React.FC = () => {
           ListFooterComponentStyle={{
             height: 80,
           }}
-          renderItem={({ item }: { item: Product }) => (
+          renderItem={({ item }) => (
             <Product>
               <ProductImage source={{ uri: item.image_url }} />
               <ProductTitleContainer>
